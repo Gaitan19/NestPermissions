ac7b96bfebf4eca6fac3810b008857e8c133748d
kenley.gaitan <kenley.gaitan@smbssolutions.com>
2024-01-18 16:45:34 -0600

    it was added somethin

--- apps/upload-files/src/files/files.controller.ts
+++ apps/upload-files/src/files/files.controller.ts
@@ -19,43 +19,54 @@ import { UpdateFileDto } from './dto/update-file.dto';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
 import { fileFilter, renameImage } from './helpers/images.helper';
+import * as fs from 'fs';
+import * as path from 'path';
 
 @Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
 
-  // @Post('upload')
-  // @UseInterceptors(FileInterceptor('file'), {
-  //   storage: diskStorage({
-  //     destination: './uploads',
-  //     filename: renameImage,
-  //   }),
-  //   fileFilter: fileFilter,
-  // })
-  // uploadFile(@UploadedFile() file: Express.Multer.File) {
-  //   console.log('file', file);
-  //   const imageBase64 = file.buffer.toString('base64');
-  //   return this.filesService.uploadFile(imageBase64);
-  // }
-
   @Post('upload')
-  @UseInterceptors(
-    FileInterceptor('file', {
-      storage: diskStorage({
-        destination: './uploads',
-        filename: renameImage,
-      }),
-      fileFilter: fileFilter,
-    }),
-  )
-  uploadFile(@UploadedFile() file: Express.Multer.File) {
-    console.log('file', file);
+  uploadFile(@Body() body: { image: string }) {
+    try {
+      const base64Image = body.image.split(';base64,').pop();
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+      const filename = `image-${uniqueSuffix}.png`;
+      const imagePath = path.join('./public/uploads', filename);
+
+      fs.writeFileSync(imagePath, base64Image, { encoding: 'base64' });
+
+      return {
+        path: imagePath,
+        filename,
+      };
+    } catch (error) {
+      console.log('error :>> ', error);
+      return {
+        error: 'Error al procesar la imagen',
+      };
+    }
   }
 
-  // @Post()
-  // create(@Body() createFileDto: CreateFileDto) {
-  //   return this.filesService.create(createFileDto);
+  // @Post('upload')
+  // @UseInterceptors(
+  //   FileInterceptor('file', {
+  //     storage: diskStorage({
+  //       destination: './public/uploads',
+  //       filename: renameImage,
+  //     }),
+  //     fileFilter: fileFilter,
+  //   }),
+  // )
+  // uploadFile(@UploadedFile() file: Express.Multer.File) {
+  //   console.log('file', file);
   // }
+
+  @Post()
+  create(@Body() createFileDto: CreateFileDto) {
+    return this.filesService.create(createFileDto);
+  }
+
   @Get()
   findAll() {
     return this.filesService.findAll();
--- apps/upload-files/src/main.ts
+++ apps/upload-files/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { UploadFilesModule } from './upload-files.module';
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(UploadFilesModule);
+  const app =
+    await NestFactory.create<NestExpressApplication>(UploadFilesModule);
+  console.log(
+    'Ruta completa del directorio estático:',
+    join(__dirname, '..', 'upload-files/public/uploads'),
+  );
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  // app.useStaticAssets(join(__dirname, 'uploads'));
 
   await app.listen(3005);
 }
